a324baca929f1936f06bd378f63f417ecbff7fc8
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-02 12:15:32 -0500

    Migrate to framer motion. Organize imports in home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 
-import BrandMission from "@/components/Blocks/BrandMission";
-import Clients from "@/components/Blocks/Clients";
-import Hero from "@/components/Blocks/Hero";
-import RequestDemo from "@/components/Blocks/RequestDemo";
-import Solutions from "@/components/Blocks/Solutions";
-import Testimonials from "@/components/Blocks/Testimonials";
+import { Hero, BrandMission, Solutions, Testimonials, Clients, RequestDemo } from "@/sections";
 
 const Home: React.FC = () => {
   return (
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,1 +1,2 @@
 export * from "./bug-hunters";
+export * from "./home";
