de6023fc2c9643821689a6cdad93faf65330c355
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-02 14:36:13 -0500

    Bug Hunters. Ecosystem section. Animations.

--- src/sections/bug-hunters/ecosystem/index.tsx
+++ src/sections/bug-hunters/ecosystem/index.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 
 import "./styles.scss";
 import data from "@/data/bug-hunters/ecosystem.json";
+
+interface EcosystemItemProps {
+  description: string;
+  icon: string;
+  title: string;
+}
+
+const EcosystemFeatureItem: React.FC<EcosystemItemProps> = ({ title, icon, description }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <motion.div
+      className="ecosystem-feature-item"
+      initial={{ backgroundColor: "#F6F7F8" }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      whileHover={{ backgroundColor: "#045DE3" }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <motion.img
+        alt={title}
+        animate={
+          isHovered
+            ? { width: "46px", height: "46px" }
+            : {
+                width: "104px",
+                height: "104px",
+              }
+        }
+        className="ecosystem-feature-icon"
+        initial={{ width: "104px", height: "104px" }}
+        src={icon}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      />
+      <motion.h3
+        animate={
+          isHovered
+            ? {
+                left: "92px",
+                top: "36px",
+                width: "calc(100% - 102px)",
+                color: "#F6F7F8",
+              }
+            : {
+                left: "40px",
+                top: "calc(100% - 60px)",
+                width: "100%",
+                color: "#02255B",
+              }
+        }
+        className="heading-7 ecosystem-feature-title"
+        initial={{
+          left: "40px",
+          top: "calc(100% - 60px)",
+          width: "100%",
+          color: "#02255B",
+        }}
+        style={{
+          position: "absolute",
+        }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        {title}
+      </motion.h3>
+    </motion.div>
+  );
+};
 
 export const Ecosystem: React.FC = () => {
   return (
@@ -11,10 +81,7 @@ export const Ecosystem: React.FC = () => {
         <p className="paragraph-md ecosystem-text">{data.subtitle}</p>
         <div className="ecosystem-feature-container">
           {data.features.map((feature) => (
-            <div key={feature.id} className="ecosystem-feature-item">
-              <img alt={feature.title} className="ecosystem-feature-icon" src={feature.icon} />
-              <h3 className="heading-7 ecosystem-feature-title">{feature.title}</h3>
-            </div>
+            <EcosystemFeatureItem key={feature.id} {...feature} />
           ))}
         </div>
       </div>
