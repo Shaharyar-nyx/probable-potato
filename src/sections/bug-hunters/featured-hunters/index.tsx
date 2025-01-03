ab93948b40d1b9d4e00a07175ec571e59c3413ac
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-03 11:19:56 -0500

    Bug Hunters. Featured hunters cards, images.

--- src/sections/bug-hunters/featured-hunters/index.tsx
+++ src/sections/bug-hunters/featured-hunters/index.tsx
@@ -13,6 +13,19 @@ export const FeaturedHunters: React.FC = () => {
         <span className="tagline featured-hunters-tagline">{data.tagline}</span>
         <h2 className="heading-1 featured-hunters-title">{data.title}</h2>
         <p className="paragraph-md featured-hunters-description">{data.description}</p>
+        <div className="featured-hunters-top-hunters-container">
+          {data["top-hunters"].map((hunter) => (
+            <div key={hunter.id} className="overflow-hidden rounded-xl">
+              <div className="relative rounded-xl">
+                <img alt={hunter.name} className="featured-hunters-top-hunters-image" src={hunter.picture} />
+                <div className="featured-hunters-top-hunters-text-container">
+                  <h3 className="heading-5 featured-hunters-top-hunters-text-heading">{hunter.name}</h3>
+                  <p className="paragraph-md featured-hunters-top-hunters-text-description">{hunter.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
