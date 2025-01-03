2beffb4e827665ef368f59455fcca788bc425326
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-03 14:08:54 -0500

    Bug Hunters. Hunters. Scroll elements.

--- src/sections/bug-hunters/featured-hunters/index.tsx
+++ src/sections/bug-hunters/featured-hunters/index.tsx
@@ -1,4 +1,5 @@
 import "./styles.scss";
+import clsx from "clsx";
 import React from "react";
 
 import data from "@/data/bug-hunters/featured-hunters.json";
@@ -35,9 +36,29 @@ export const FeaturedHunters: React.FC = () => {
         <div className="featured-hunters-hunters-container">
           {data.hunters.map((hunter) => (
             <div key={hunter.id} className="featured-hunters-hunters-item">
-              <div>Hello world</div>
-              <div>Hello world</div>
-              <div>Hello world</div>
+              <div className="featured-hunters-hunters-position-container">
+                <p className="paragraph-md featured-hunters-hunters-position-text">#{hunter.position}</p>
+                <p className="paragraph-md featured-hunters-hunters-position-text">{hunter.status}</p>
+              </div>
+              <div className="featured-hunters-hunters-headline-container">
+                <h3 className="heading-7 featured-hunters-hunters-headline-title">{hunter.title}</h3>
+                <div className="featured-hunters-hunters-headline-avatar-container">
+                  <img alt={hunter.name} className="h-8 w-8 rounded-full object-cover" src={hunter.avatar} />
+                  <p className="paragraph-sm featured-hunters-hunters-headline-text">{hunter.name}</p>
+                </div>
+              </div>
+              <div className="featured-hunters-hunters-score-container">
+                <p
+                  className={clsx("paragraph-sm featured-hunters-hunters-score-score", {
+                    "bg-[#1e49af] text-neutral-50": hunter.score === "low" || hunter.score === "high",
+                    "bg-[#dfdca6] text-primary-800": hunter.score === "medium",
+                    "bg-[#dd6c6c] text-neutral-50": hunter.score === "critical",
+                  })}
+                >
+                  {hunter.score}
+                </p>
+                <p className="paragraph-sm featured-hunters-hunters-score-text">CVSS Score: {hunter["cvss_score"]}</p>
+              </div>
             </div>
           ))}
         </div>
