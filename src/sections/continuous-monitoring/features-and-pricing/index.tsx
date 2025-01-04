ffe2d58e815eb5899a6dd10650ed615fcceb072d
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-04 11:36:48 -0500

    Borders and layout fixes.

--- src/sections/continuous-monitoring/features-and-pricing/index.tsx
+++ src/sections/continuous-monitoring/features-and-pricing/index.tsx
@@ -1,11 +1,12 @@
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { CheckBadgeIcon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
 import React from "react";
 
 import "./styles.scss";
 import { Button, IconRenderer, Tooltip } from "@/components";
 import data from "@/data/continuous-monitoring/features-and-pricing.json";
 import { formatToCurrency } from "@/lib";
-
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 export const FeaturesAndPricing: React.FC = () => {
   return (
@@ -22,7 +23,7 @@ export const FeaturesAndPricing: React.FC = () => {
                   {feature.sub_features.map((subFeature) => (
                     <div key={subFeature.id} className="features-and-pricing-sub-feature-item">
                       <div className="features-and-pricing-sub-feature-icon-container">
-                        <IconRenderer className="h-5 w-5 text-primary-800" iconName="InformationCircleIcon" />
+                        <IconRenderer className="h-5 w-5 text-neutral-50" iconName="InformationCircleIcon" />
                       </div>
                       <div>
                         <h4 className="paragraph-lg features-and-pricing-sub-feature-title">{subFeature.title}</h4>
@@ -56,27 +57,53 @@ export const FeaturesAndPricing: React.FC = () => {
           </div>
         </div>
         <div className="features-and-pricing-content">
-          <table className="w-full">
+          <table className="relative w-full">
             <thead>
               <tr>
-                <th className="paragraph-xl inline-block px-7 py-2 text-left font-semibold text-primary-800">
-                  {data.package_features.title}
+                <th className="paragraph-xl inline-block text-left font-semibold text-primary-800">
+                  <div className="rounded-tl-xl rounded-tr-xl border border-b-neutral-50 border-l-neutral-100 border-r-neutral-100 border-t-neutral-100 px-7 py-2">
+                    {data.package_features.title}
+                  </div>
                 </th>
               </tr>
             </thead>
             <tbody>
-              {data.package_features.items.map((packageItem) => (
+              {data.package_features.items.map((packageItem, index) => (
                 <tr key={packageItem.id} className="flex">
-                  <td className="paragraph-sm flex w-[64%] gap-2 px-7 py-4 text-left text-primary-800">
-                    <span>{packageItem.title}</span>
-                    {packageItem.tooltip !== undefined && (
-                      <Tooltip content={packageItem.tooltip}>
-                        <InformationCircleIcon className="h-5 w-5 text-primary-800" />
-                      </Tooltip>
-                    )}
+                  <td className="w-[64%]">
+                    <div
+                      className={clsx(
+                        "paragraph-sm flex w-full gap-2 border-b border-l border-b-neutral-100 border-l-neutral-100 px-7 py-4 text-left text-primary-800",
+                        {
+                          "border-t border-t-neutral-100": index === 0,
+                          "rounded-bl-xl": index === data.package_features.items.length - 1,
+                        },
+                      )}
+                    >
+                      <span>{packageItem.title}</span>
+                      {packageItem.tooltip !== undefined && (
+                        <Tooltip content={packageItem.tooltip}>
+                          <InformationCircleIcon className="h-5 w-5 text-primary-800" />
+                        </Tooltip>
+                      )}
+                    </div>
                   </td>
-                  <td className="paragraph-sm w-[36%] px-7 py-4 text-center text-primary-800">
-                    {packageItem.feature ?? packageItem.badge}
+                  <td className="w-[36%]">
+                    <div
+                      className={clsx(
+                        "paragraph-sm flex w-full justify-center gap-2 border-b border-r border-b-neutral-100 border-r-neutral-100 px-7 py-4 text-left text-primary-800",
+                        {
+                          "rounded-tr-xl border-t border-t-neutral-100": index === 0,
+                          "rounded-br-xl": index === data.package_features.items.length - 1,
+                        },
+                      )}
+                    >
+                      {packageItem.badge ? (
+                        <CheckBadgeIcon className="h-5 w-5 text-primary-800" />
+                      ) : (
+                        <span>{packageItem.feature}</span>
+                      )}
+                    </div>
                   </td>
                 </tr>
               ))}
