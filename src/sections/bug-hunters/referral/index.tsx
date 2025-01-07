d70404246477c5e7a47ede9f0586be172bafda2e
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-07 15:40:25 -0500

    Bug Hunters. Send a referral overlay.

--- src/sections/bug-hunters/referral/index.tsx
+++ src/sections/bug-hunters/referral/index.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
 
 import { Button, IconRenderer } from "@/components";
 import data from "@/data/bug-hunters/referral.json";
@@ -6,6 +9,16 @@ import data from "@/data/bug-hunters/referral.json";
 import "./styles.scss";
 
 export const Referral: React.FC = () => {
+  const [showReferralScreen, setShowReferralScreen] = useState(false);
+
+  const handleOpenReferral = () => {
+    setShowReferralScreen(true);
+  };
+
+  const handleCloseReferral = () => {
+    setShowReferralScreen(false);
+  };
+
   return (
     <section className="referral-parent-container">
       <div className="referral-container">
@@ -14,7 +27,7 @@ export const Referral: React.FC = () => {
           <h2 className="heading-1 mb-5 font-bold text-primary-800">{data.title}</h2>
           <p className="paragraph-md mb-2 font-bold text-primary-800">{data.subtitle}</p>
           <p className="paragraph-md mb-5 text-primary-800">{data.text}</p>
-          <Button>Send a Referral</Button>
+          <Button onClick={handleOpenReferral}>Send a Referral</Button>
         </div>
         <div className="referral-container-item grid grid-cols-2 gap-6">
           {data.features.map((feature) => (
@@ -30,6 +43,24 @@ export const Referral: React.FC = () => {
           ))}
         </div>
       </div>
+
+      {/* AnimatePresence and Motion for Referral Screen */}
+      <AnimatePresence>
+        {showReferralScreen && (
+          <motion.div
+            animate={{ clipPath: "inset(0 0 0 0%)" }}
+            className="referral-overlay"
+            exit={{ clipPath: "inset(0 0 0 100%)" }}
+            initial={{ clipPath: "inset(0 0 0 100%)" }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+          >
+            <div className="overlay-content">
+              <h1 className="text-white">Hello World</h1>
+              <Button onClick={handleCloseReferral}>Close</Button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
