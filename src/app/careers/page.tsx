a3687a1973bbe28d375ebf6e25fa2888b8ed9246
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-08 18:21:38 -0500

    Merge branch 'development' into bug-hunters
    # Conflicts:
    #	src/app/bug-hunters/page.tsx
    #	src/sections/bug-hunters/index.ts
    #	src/sections/contact-us/contact-form/styles.module.scss

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { Header } from "@/components";
 import benefitsData from "@/data/careers/benefits.json";
 import clientsContent from "@/data/careers/clients.json";
