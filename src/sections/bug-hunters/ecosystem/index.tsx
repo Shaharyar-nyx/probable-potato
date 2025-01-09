b2bd51cebc519359b5088628753c2c3484eb7255
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-09 15:57:44 -0500

    Bug Hunters. Ecosystem. Responsiveness.

--- src/sections/bug-hunters/ecosystem/index.tsx
+++ src/sections/bug-hunters/ecosystem/index.tsx
@@ -5,15 +5,30 @@ import React, { useState } from "react";
 
 import "./styles.scss";
 import data from "@/data/bug-hunters/ecosystem.json";
+import { useScreenWidth } from "@/hooks";
 
 interface EcosystemItemProps {
   description: string;
-  icon: string;
+  icon_dark: string;
+  icon_light: string;
   title: string;
 }
 
-const EcosystemFeatureItem: React.FC<EcosystemItemProps> = ({ title, icon, description }) => {
+const XL_BOX_PADDING = 40;
+const LG_BOX_PADDING = 20;
+const XL_ICON_SIZE_BIG = 104;
+const XL_ICON_SIZE_SMALL = 46;
+const LG_ICON_SIZE_BIG = 82;
+const LG_ICON_SIZE_SMALL = 38;
+
+const EcosystemFeatureItem: React.FC<EcosystemItemProps> = ({ title, icon_light, icon_dark, description }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const screenWidth = useScreenWidth();
+
+  // Determine icon sizes and padding based on screen width
+  const BOX_PADDING = screenWidth > 1280 ? XL_BOX_PADDING : LG_BOX_PADDING;
+  const ICON_SIZE_BIG = screenWidth > 1280 ? XL_ICON_SIZE_BIG : LG_ICON_SIZE_BIG;
+  const ICON_SIZE_SMALL = screenWidth > 1280 ? XL_ICON_SIZE_SMALL : LG_ICON_SIZE_SMALL;
 
   return (
     <motion.div
@@ -24,42 +39,59 @@ const EcosystemFeatureItem: React.FC<EcosystemItemProps> = ({ title, icon, descr
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <motion.img
-        alt={title}
-        animate={
-          isHovered
-            ? { width: "46px", height: "46px" }
-            : {
-                width: "104px",
-                height: "104px",
-              }
-        }
-        className="ecosystem-feature-icon"
-        initial={{ width: "104px", height: "104px" }}
-        src={icon}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
-      />
+      {/* Dark Icon */}
+      <div className="relative">
+        <motion.img
+          alt={`${title} Dark Icon`}
+          animate={
+            isHovered
+              ? { width: ICON_SIZE_SMALL, height: ICON_SIZE_SMALL, opacity: 0 }
+              : { width: ICON_SIZE_BIG, height: ICON_SIZE_BIG, opacity: 1 }
+          }
+          className="ecosystem-feature-icon"
+          initial={{ width: ICON_SIZE_BIG, height: ICON_SIZE_BIG, opacity: 1 }}
+          src={icon_dark}
+          style={{ position: "absolute", top: 0, left: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        />
+
+        {/* Light Icon */}
+        <motion.img
+          alt={`${title} Light Icon`}
+          animate={
+            isHovered
+              ? { width: ICON_SIZE_SMALL, height: ICON_SIZE_SMALL, opacity: 1 }
+              : { width: ICON_SIZE_BIG, height: ICON_SIZE_BIG, opacity: 0 }
+          }
+          className="ecosystem-feature-icon"
+          initial={{ width: ICON_SIZE_BIG, height: ICON_SIZE_BIG, opacity: 0 }}
+          src={icon_light}
+          style={{ position: "absolute", top: 0, left: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        />
+      </div>
+
       <motion.h3
         animate={
           isHovered
             ? {
-                left: "92px",
-                top: "36px",
+                left: BOX_PADDING + 52, // Adding 52px for icon offset
+                top: BOX_PADDING - 4,
                 width: "calc(100% - 110px)",
                 color: "#F6F7F8",
               }
             : {
-                left: "40px",
+                left: BOX_PADDING,
                 top: "calc(100% - 60px)",
-                width: "100%",
+                width: "80%",
                 color: "#02255B",
               }
         }
-        className="heading-7 ecosystem-feature-title"
+        className="ecosystem-feature-title"
         initial={{
-          left: "40px",
+          left: BOX_PADDING,
           top: "calc(100% - 60px)",
-          width: "100%",
+          width: "80%",
           color: "#02255B",
         }}
         style={{
@@ -69,14 +101,20 @@ const EcosystemFeatureItem: React.FC<EcosystemItemProps> = ({ title, icon, descr
       >
         {title}
       </motion.h3>
+
       <AnimatePresence>
         {isHovered && (
           <motion.p
             animate={{ opacity: 1, y: 0 }}
-            className="paragraph-md ecosystem-feature-description"
+            className="ecosystem-feature-description"
             exit={{ opacity: 0, y: 20 }}
             initial={{ opacity: 0, y: 20 }}
-            style={{ display: isHovered ? "block" : "none" }}
+            style={{
+              display: isHovered ? "block" : "none",
+              position: "absolute",
+              bottom: "40px",
+              left: BOX_PADDING,
+            }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
           >
             {description}
