407415c7006799412facf18bc3e6d8a985a0119e
gustavo.nunezortiz <gustavo.nunez@hylinkgroup.com>
2025-01-09 16:36:12 -0500

    Bug Hunters. Ecosystem. Responsiveness.

--- src/hooks/useScreenWidth.ts
+++ src/hooks/useScreenWidth.ts
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
 
 export const useScreenWidth = () => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenWidth, setScreenWidth] = useState<number>(
+    typeof window !== "undefined" ? window.innerWidth : 0, // Fallback for SSR
+  );
 
   useEffect(() => {
+    if (typeof window === "undefined") return; // Exit if not on the client
+
     const handleResize = () => setScreenWidth(window.innerWidth);
     window.addEventListener("resize", handleResize);
 
+    // Cleanup event listener on unmount
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
