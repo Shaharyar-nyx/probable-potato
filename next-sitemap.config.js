90fedd4287743fc82e816c3ffce484f7836b58ed
Teffany Mae Reyes <teffany.reyes@Admins-MacBook-Air.local>
2025-02-27 07:30:54 +0800

    feat: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://cyberbay.tech',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${process.env.NEXT_PUBLIC_SITE_URL}/server-sitemap.xml`,
+    ],
+  },
+}
