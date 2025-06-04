6c62b7f53ce47d0c2d5c48b8357ca4fb93e3cb63
UserName <email@email.com>
2025-06-04 17:36:49 +0700

    Init event campain 2025

---
+++ src/app/event/page.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+import { Header } from "@/components";
+import { Introduction, Accordion } from "@/sections";
+import { PageBuilder } from "@/components/PageBuilder";
+import { getPageBySlug, STRAPI_ASSETS } from "@/lib";
+import { Metadata } from "next";
+
+const blockComponents: Record<string, React.FC<any>> = {
+  hero_section: Header,
+  simple_section: Introduction,
+  multi_card_section: Accordion,
+};
+
+async function getPageData() {
+  return getPageBySlug("privacy-policy");
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await getPageData();
+  if (!data?.seo) {
+    return {
+      title: "Cyberbay",
+      description: "",
+      openGraph: {
+        title: "Cyberbay",
+        description: "",
+        type: "website",
+        siteName: "Cyberbay",
+        locale: "en_US",
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: "Cyberbay",
+        description: "",
+      },
+    };
+  }
+
+  return {
+    title: data.seo.title,
+    description: data.seo.meta_description,
+    keywords: data.seo.keywords,
+    robots: {
+      index: !data.seo.no_index,
+      follow: !data.seo.no_follow,
+    },
+    openGraph: {
+      title: data.seo.title,
+      description: data.seo.meta_description,
+      images: [{ url: STRAPI_ASSETS + data.seo.og_image?.data?.attributes?.url || "/favicon.ico" }],
+      url: data.seo.canonical_url,
+      type: "website",
+      siteName: "Cyberbay",
+      locale: "en_US",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: data.seo.title,
+      description: data.seo.meta_description,
+      images: [STRAPI_ASSETS + data.seo.og_image?.data?.attributes?.url || "/favicon.ico"],
+    },
+  };
+}
+
+async function Event2025Page() {
+  const data = await getPageData();
+  if (!data?.blocks) {
+    return null;
+  }
+
+  return (
+    <PageBuilder blockComponents={blockComponents} blocks={data.blocks} />
+  );
+};
+
+export default Event2025Page;
