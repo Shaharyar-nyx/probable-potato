74cca82d3ad03db9aafcc90e6d774e9b8635c52d
faheem-ops <faheem@cyberbay.tech>
2025-11-30 19:13:35 +0500

    temporary transport non-ssl allow

--- next.config.ts
+++ next.config.ts
@@ -162,7 +162,7 @@ const securityHeadersConfig = (phase: string) => {
       value: cspHeader().replace(/\n/g, ""),
     },
     { key: "X-DNS-Prefetch-Control", value: "on" },
-    { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+    //{ key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
     { key: "X-Frame-Options", value: "DENY" },
     { key: "X-Content-Type-Options", value: "nosniff" },
     { key: "X-XSS-Protection", value: "1; mode=block" },
